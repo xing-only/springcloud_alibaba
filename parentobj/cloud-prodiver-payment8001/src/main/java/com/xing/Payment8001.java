e4adde2032a87740d0c3dee1df2a9df48a83a65e
xing <1515113603qq.com>
2021-04-23 11:02:34 +0800

    服务发现，查看注册中心中，注册服务的服务详细信息

--- parentobj/cloud-prodiver-payment8001/src/main/java/com/xing/Payment8001.java
+++ parentobj/cloud-prodiver-payment8001/src/main/java/com/xing/Payment8001.java
@@ -2,6 +2,7 @@ package com.xing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import javax.swing.*;
@@ -13,6 +14,7 @@ import javax.swing.*;
  **/
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient  //服务发现
 public class Payment8001 {
     public static void main(String[] args){
         SpringApplication.run(Payment8001.class, args);
--- parentobj/cloud-prodiver-payment8001/src/main/java/com/xing/controller/PayMentController.java
+++ parentobj/cloud-prodiver-payment8001/src/main/java/com/xing/controller/PayMentController.java
@@ -7,7 +7,11 @@ import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @Description:
@@ -24,6 +28,9 @@ public class PayMentController {
 
     @Autowired
     private PayMentService payMentService;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     /**
      * 新增
@@ -45,5 +52,20 @@ public class PayMentController {
         log.info("端口号：" + port);
         PayMent entity = this.payMentService.getPayMentById(id);
         return new CommonResult<PayMent>(200,"success",entity);
+    }
+
+    @GetMapping("/getDiscovery")
+    public CommonResult getDiscovery(){
+        List<String> services = discoveryClient.getServices();
+        services.forEach(str ->{
+            log.info(str);
+        });
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVER");
+        instances.forEach(entity ->{
+            log.info(entity.getServiceId() + "\t" + entity.getHost() + "\t" + entity.getPort() + "\t" + entity.getUri());
+        });
+
+        return new CommonResult(200,"success",discoveryClient);
     }
 }
