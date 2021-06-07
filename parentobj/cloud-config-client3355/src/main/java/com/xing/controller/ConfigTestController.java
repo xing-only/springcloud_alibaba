b8707ecb489e3a678863f1ba84e7f4f69d8dfa12
xing <1515113603qq.com>
2021-06-07 16:29:46 +0800

    spring eureka config client  动态刷新配置信息，添加注解，并在bootstrap.yml中配置 暴露监控端点

--- parentobj/cloud-config-client3355/src/main/java/com/xing/controller/ConfigTestController.java
+++ parentobj/cloud-config-client3355/src/main/java/com/xing/controller/ConfigTestController.java
@@ -2,6 +2,7 @@ package com.xing.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("/config")
+@RefreshScope
 public class ConfigTestController {
 
     @Value("${config.info}")
